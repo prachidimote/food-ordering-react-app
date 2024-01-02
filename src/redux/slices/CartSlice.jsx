e71710715c573fe54d56e4087ade8f5845b797df
prachidimote <81797753+prachidimote@users.noreply.github.com>
2024-01-02 11:34:36 +0530

    updated quantity of food item

--- src/redux/slices/CartSlice.jsx
+++ src/redux/slices/CartSlice.jsx
@@ -8,7 +8,17 @@ const CartSlice = createSlice({
     },
     reducers: {
         addToCart : (state, action) => {
-            state.cart.push(action.payload);
+            const existingItem = state.cart.find(
+                (item) => item.id === action.payload.id);
+                if(existingItem){
+                    state.cart = state.cart.map((item) => 
+                    item.id === action.payload.id ? { ...item, qty: item.qty + 1} : item
+                    );
+
+                }else {
+                    state.cart.push(action.payload);
+                }
+            
 
         },
         removeFromCart: (state, action) =>{
