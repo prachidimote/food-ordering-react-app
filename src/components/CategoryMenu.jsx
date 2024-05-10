e87ea1de49df31f2d7fd8f247b796cc09eaae4c9
prachidimote <81797753+prachidimote@users.noreply.github.com>
2024-05-10 10:10:18 +0530

    added sass dev dependency

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -1,13 +1,39 @@
+import { useEffect, useState } from 'react';
+import FoodData from '../data/FoodData';
+import { useDispatch } from 'react-redux';
+import { setCategory } from '../redux/slices/CategorySlice';
 const CategoryMenu = () => {
+  const [categories, setCategories] = useState([]);
+  const listUniqueCategories  = () => {
+    const uniqueCategories = [...new Set(FoodData.map((food) => food.category))];
+    setCategories(uniqueCategories);
+    console.log(uniqueCategories); // ['Lunch', 'Breakfast', 'Dinner', 'Snacks']
+  };
+
+  useEffect(() => {
+    listUniqueCategories();
+  },[]);
+
+  const dispatch = useDispatch();
     return (
       <div className="mx-6">
           <h3>Find the best food</h3>  
       <div className="my-3 flex gap-3 overflow-x-scroll scroll-smooth lg:overflow-hidden">
-          <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-600 hover:text-white">All</button>
-          <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-600 hover:text-white">Lunch</button>
-          <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-600 hover:text-white">Breakfast</button>
-          <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-600 hover:text-white">Dinner</button>
-          <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-600 hover:text-white">Snacks</button>
+      <button 
+              onClick={() => dispatch(setCategory("All"))}
+              className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-600 hover:text-white">All
+              </button>
+        {
+          categories.map((category, index) => {
+            return (
+              <button 
+              onClick={() => dispatch(setCategory(category))}
+              key={index} 
+              className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-600 hover:text-white">{category}
+              </button>
+            )
+          })
+        }   
       </div>
       </div>
     )
